5b2f58c8ac71c56edf930fe2483c3ed37b81ccd0
Asad Waseem <asadwaseem299@gmail.com>
2026-04-22 15:56:24 +0500

    Merged team code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Hero from '../components/sections/Hero';
 import WhyChooseUs from '../components/sections/WhyChooseUs';
+import PortfolioSection from '../components/sections/PortfolioSection'; 
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <WhyChooseUs />
+      <PortfolioSection /> {/* Ise yahan render karein */}
+      {/* Jab Navbar aur Footer ban jayein toh wo bhi yahan add honge */}
     </main>
   );
 }
